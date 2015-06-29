da3ed166325f7cc6134572cd71cc7b396a9b20f6
Daniela Baron <baron.daniela@gmail.com>
2015-06-29 19:06:31 -0400

    Initial commit

---
+++ EXAMPLES/triangle.js
@@ -0,0 +1,44 @@
+
+var gl;
+var points;
+
+window.onload = function init()
+{
+    var canvas = document.getElementById( "gl-canvas" );
+    
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    
+    var vertices = [-1, -1, 0, 1, 1, -1];
+
+    //  Configure WebGL
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    
+    //  Load shaders and initialize attribute buffers
+    
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+    
+    // Load the data into the GPU
+    
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    // Associate out shader variables with our data buffer
+    
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    render();
+};
+
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Interactive Computer Graphics with WebGL
+
+> Learning WebGL from Coursera, University of New Mexico [course](https://www.coursera.org/course/webgl)
+
+## Initial Setup
+
+Download `Common` and `Examples` from instructor's [website](http://www.cs.unm.edu/~angel/COURSERA/CODE/)
+
+```bash
+wget -r -nH --cut-dirs=4 --no-parent --reject="index.html*" http://www.cs.unm.edu/~angel/COURSERA/CODE/Common/
+wget -r -nH --cut-dirs=4 --no-parent --reject="index.html*" http://www.cs.unm.edu/~angel/COURSERA/CODE/EXAMPLES/
+```
