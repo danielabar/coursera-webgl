091446ddc35c3b163bd6656eac9d8b9d86a5dd38
Daniela Baron <baron.daniela@gmail.com>
2015-07-01 15:24:18 -0400

    Deployed by grunt gh-pages

---
+++ experiments/multiple/triangle.js
@@ -0,0 +1,53 @@
+var gl;       // WebGL context
+
+var render = function(clear, bufferId, program) {
+  var vPosition;
+
+  // Clear canvas if requested
+  if (clear) {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+  }
+
+  // Outer program already calls this, but still needed here again
+  gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+
+  // Associate shader variables with data buffer
+  vPosition = gl.getAttribLocation( program, 'vPosition' );
+  gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+  gl.enableVertexAttribArray( vPosition );
+
+  // Draw triangle
+  gl.drawArrays( gl.TRIANGLES, 0, 3 );
+};
+
+window.onload = function init() {
+
+  // Initialize WebGL context
+  var canvas = document.getElementById( 'gl-canvas' );
+  gl = WebGLUtils.setupWebGL( canvas );
+  if ( !gl ) { alert( 'WebGL isnt available' ); }
+
+  // Setup canvas
+  gl.viewport( 0, 0, canvas.width, canvas.height );
+  gl.clearColor( 0.15, 0.17, 0.15, 0.4 );
+
+  // Load shaders
+  var program = initShaders( gl, 'vertex-shader', 'fragment-shader' );
+  gl.useProgram( program );
+
+  // Load triangle 1 onto the GPU
+  var triangle1 = new Float32Array([-1, -1, 0, 0, 1, -1]);
+  var bufferId1 = gl.createBuffer();
+  gl.bindBuffer( gl.ARRAY_BUFFER, bufferId1 );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(triangle1), gl.STATIC_DRAW );
+
+  // Load triangle 2 onto the GPU
+  var triangle2 = new Float32Array([-1, 1, 0, 0, 1, 1]);
+  var bufferId2 = gl.createBuffer();
+  gl.bindBuffer( gl.ARRAY_BUFFER, bufferId2 );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(triangle2), gl.STATIC_DRAW );
+
+  // Draw the triangles
+  render(true, bufferId1, program);
+  render(false, bufferId2, program);
+};
---
+++ experiments/position/triangle.js
@@ -0,0 +1,39 @@
+var gl;       // WebGL context
+
+var render = function() {
+  gl.clear( gl.COLOR_BUFFER_BIT );
+  gl.drawArrays( gl.TRIANGLES, 0, 3 );
+};
+
+window.onload = function init() {
+  var canvas = document.getElementById( 'gl-canvas' );
+
+  gl = WebGLUtils.setupWebGL( canvas );
+  if ( !gl ) { alert( 'WebGL isnt available' ); }
+
+  // Setup data
+  // var vertices = [-1, -1, 0, 1, 1, -1];
+  var vertices = [-0.5, -0.5, 0.5, 0.5, 0.5, -0.5];
+
+  //  Configure WebGL
+  gl.viewport( 0, 0, canvas.width, canvas.height );
+
+  // Set canvas background color
+  gl.clearColor( 0.15, 0.17, 0.15, 0.8 );
+
+  //  Load shaders and initialize attribute buffers
+  var program = initShaders( gl, 'vertex-shader', 'fragment-shader' );
+  gl.useProgram( program );
+
+  // Load the data into the GPU
+  var bufferId = gl.createBuffer();
+  gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+  // Associate shader variables with data buffer
+  var vPosition = gl.getAttribLocation( program, 'vPosition' );
+  gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+  gl.enableVertexAttribArray( vPosition );
+
+  render();
+};
