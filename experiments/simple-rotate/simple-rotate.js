1cc71adabe66af1671320a5d31b1755d9a9429bc
Daniela Baron <baron.daniela@gmail.com>
2015-07-04 15:11:36 -0400

    experiment - rotate triangle by 30 degrees

--- experiments/simple-rotate/simple-rotate.js
+++ experiments/simple-rotate/simple-rotate.js
@@ -7,28 +7,33 @@ var render = function() {
   gl.drawArrays( gl.TRIANGLES, 0, 3 );
 };
 
-var rotate = function(point) {
-
+var rotate = function(vec2Point, theta) {
+  var originalX = vec2Point[0];
+  var originalY = vec2Point[1];
+  var newX = (originalX * Math.cos(theta)) - (originalY * Math.sin(theta));
+  var newY = (originalX * Math.sin(theta)) + (originalY * Math.cos(theta));
+  return vec2(newX, newY);
 };
 
 window.onload = function init() {
+
   // init
   var canvas = document.getElementById( 'gl-canvas' );
   gl = WebGLUtils.setupWebGL( canvas );
   if ( !gl ) { alert( 'WebGL isn\'t available' ); }
 
-  // define triangle points
-  // var vertices = new Float32Array([
-  //   -0.5, -0.5, // bottom left
-  //   0, 0.5,   // top center
-  //   0.5, -0.5    // bottom right
-  // ]);
+  var originalTriangle = [
+    vec2(-0.5, -0.5),
+    vec2(0, 0.5),
+    vec2(0.5, -0.5)
+  ];
 
-  var triangle = new Float32Array([
-    -0.5, -0.5,
-    0, 0.5,
-    0.5, -0.5
-  ]);
+  var theta = 30;
+  var rotatedTriangle = [
+    rotate(originalTriangle[0], theta),
+    rotate(originalTriangle[1], theta),
+    rotate(originalTriangle[2], theta)
+  ];
 
   // configure display
   gl.viewport( 0, 0, canvas.width, canvas.height );
@@ -41,7 +46,7 @@ window.onload = function init() {
   // load the data into the GPU
   var bufferId = gl.createBuffer();
   gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-  gl.bufferData( gl.ARRAY_BUFFER, triangle, gl.STATIC_DRAW );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(rotatedTriangle), gl.STATIC_DRAW );
 
   // associate shader variables with data buffer
   var vPosition = gl.getAttribLocation( program, 'vPosition' );
