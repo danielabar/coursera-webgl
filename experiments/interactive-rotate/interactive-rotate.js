1aa119f513ca2185e55b1bf780cad9e7b383e9b0
Daniela Baron <baron.daniela@gmail.com>
2015-07-04 15:52:40 -0400

    experiment - start on interactive rotate

---
+++ experiments/interactive-rotate/interactive-rotate.js
@@ -0,0 +1,70 @@
+'use strict';
+
+var gl;
+
+var originalTriangle = [
+  vec2(-0.5, -0.5),
+  vec2(0, 0.5),
+  vec2(0.5, -0.5)
+];
+
+var render = function() {
+  gl.clear( gl.COLOR_BUFFER_BIT );
+  gl.drawArrays( gl.TRIANGLES, 0, 3 );
+};
+
+var rotate = function(vec2Point, theta) {
+  var originalX = vec2Point[0];
+  var originalY = vec2Point[1];
+  var newX = (originalX * Math.cos(theta)) - (originalY * Math.sin(theta));
+  var newY = (originalX * Math.sin(theta)) + (originalY * Math.cos(theta));
+  return vec2(newX, newY);
+};
+
+var doRotate = function(evt) {
+  evt.preventDefault();
+  var input = document.getElementById('theta');
+  console.log(input.checkValidity());
+  var rotationDegrees = input.valueAsNumber;
+  console.log(rotationDegrees);
+};
+
+window.onload = function init() {
+
+  // register event handlers
+  document.getElementById('rotate').addEventListener('click', doRotate);
+
+  // init
+  var canvas = document.getElementById( 'gl-canvas' );
+  gl = WebGLUtils.setupWebGL( canvas );
+  if ( !gl ) { alert( 'WebGL isn\'t available' ); }
+
+
+
+  // var theta = 30;
+  // var rotatedTriangle = [
+  //   rotate(originalTriangle[0], theta),
+  //   rotate(originalTriangle[1], theta),
+  //   rotate(originalTriangle[2], theta)
+  // ];
+
+  // configure display
+  gl.viewport( 0, 0, canvas.width, canvas.height );
+  gl.clearColor( 0, 0, 0, 1.0 );
+
+  // load shaders
+  var program = initShaders( gl, 'vertex-shader', 'fragment-shader' );
+  gl.useProgram( program );
+
+  // load the data into the GPU
+  var bufferId = gl.createBuffer();
+  gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+  gl.bufferData( gl.ARRAY_BUFFER, flatten(originalTriangle), gl.STATIC_DRAW );
+
+  // associate shader variables with data buffer
+  var vPosition = gl.getAttribLocation( program, 'vPosition' );
+  gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+  gl.enableVertexAttribArray( vPosition );
+
+  render();
+};
