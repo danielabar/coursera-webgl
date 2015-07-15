2c572e15877c414fa6d11fa89a90a1d417da8ff4
Daniela Baron <baron.daniela@gmail.com>
2015-07-15 19:22:02 -0400

    maxwell2 cleanup unused code

--- experiments/maxwell2/maxwell2.js
+++ experiments/maxwell2/maxwell2.js
@@ -1,40 +1,55 @@
-var gl;       // WebGL context
-var points;   // Data to be sent to GPU
+(function(window) {
+  'use strict';
 
-// Execute init function only after the browser window has finished loading
-window.onload = function init()
-{
-    var canvas = document.getElementById( "gl-canvas" );
+  var gl;
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
-
-    // Setup data
-    var vertices = [-1, -1, 0, 1, 1, -1];
-
-    //  Configure WebGL
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-
-    //  Load shaders and initialize attribute buffers
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-
-    // Load the data into the GPU
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
-
-    // Associate shader variables with data buffer
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-
-    render();
-};
-
-
-function render() {
+  var render = function() {
     gl.clear( gl.COLOR_BUFFER_BIT );
     gl.drawArrays( gl.TRIANGLES, 0, 3 );
-}
+  };
+
+  var Maxwell = {
+
+    init: function() {
+      var canvas = document.getElementById( 'gl-canvas' );
+      gl = WebGLUtils.setupWebGL( canvas );
+      if ( !gl ) { alert( 'WebGL isn\'t available' ); }
+
+      // Setup data
+      var vertices = [-1, -1, 0, 1, 1, -1];
+
+      //  Configure WebGL
+      gl.viewport( 0, 0, canvas.width, canvas.height );
+      gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+
+      //  Load shaders and initialize attribute buffers
+      var program = initShaders( gl, 'vertex-shader', 'fragment-shader' );
+      gl.useProgram( program );
+
+      // Load vertex data into the GPU
+      var bufferId = gl.createBuffer();
+      gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+      gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+      // Associate shader variables with vertex data buffer
+      var vPosition = gl.getAttribLocation( program, 'vPosition' );
+      gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+      gl.enableVertexAttribArray( vPosition );
+
+      render();
+    }
+
+  };
+
+  window.Maxwell = Maxwell;
+
+})(window);
+
+(function() {
+  'use strict';
+
+  document.addEventListener('DOMContentLoaded', function() {
+    window.Maxwell.init();
+  });
+
+})();
