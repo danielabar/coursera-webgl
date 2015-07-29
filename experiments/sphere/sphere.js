7bb0ab56668609325c871534738403c8d76c900d
Daniela Baron <baron.daniela@gmail.com>
2015-07-29 19:59:49 -0400

    Deployed by grunt gh-pages

--- experiments/sphere/sphere.js
+++ experiments/sphere/sphere.js
@@ -1,7 +1,7 @@
 /**
  * App
  */
-(function(window) {
+(function(window, ColorUtils) {
   'use strict';
 
   var gl,
@@ -10,6 +10,8 @@
     _vertices = [],
     _vertexColors = [],
     _indices = [],
+    _color = vec4(1.0, 0.0, 0.0, 1.0),
+    _colorLoc,
     _theta = [30, 60, 0],
     _thetaLoc,
     _scale = [1, 1, 1],
@@ -22,41 +24,38 @@
     var lats = 30,
       longs = 30;
 
-      for (var latNumber = 0; latNumber <= lats; ++latNumber) {
-        for (var longNumber = 0; longNumber <= longs; ++longNumber) {
-            var theta = latNumber * Math.PI / lats;
-            var phi = longNumber * 2 * Math.PI / longs;
-            var sinTheta = Math.sin(theta);
-            var sinPhi = Math.sin(phi);
-            var cosTheta = Math.cos(theta);
-            var cosPhi = Math.cos(phi);
+    for (var latNumber = 0; latNumber <= lats; ++latNumber) {
+      for (var longNumber = 0; longNumber <= longs; ++longNumber) {
+        var theta = latNumber * Math.PI / lats;
+        var phi = longNumber * 2 * Math.PI / longs;
+        var sinTheta = Math.sin(theta);
+        var sinPhi = Math.sin(phi);
+        var cosTheta = Math.cos(theta);
+        var cosPhi = Math.cos(phi);
 
-            var x = cosPhi * sinTheta;
-            var y = cosTheta;
-            var z = sinPhi * sinTheta;
+        var x = cosPhi * sinTheta;
+        var y = cosTheta;
+        var z = sinPhi * sinTheta;
 
-            _vertices.push(radius * x);
-            _vertices.push(radius * y);
-            _vertices.push(radius * z);
-
-            _vertexColors.push(vec4( Math.random(), Math.random(), Math.random(), 1.0 ));
-        }
+        _vertices.push(radius * x);
+        _vertices.push(radius * y);
+        _vertices.push(radius * z);
+      }
     }
 
     for (var latNumberI = 0; latNumberI < lats; ++latNumberI) {
-        for (var longNumberI = 0; longNumberI < longs; ++longNumberI) {
-            var first = (latNumberI * (longs+1)) + longNumberI;
-            var second = first + longs + 1;
-            _indices.push(first);
-            _indices.push(second);
-            _indices.push(first+1);
+      for (var longNumberI = 0; longNumberI < longs; ++longNumberI) {
+        var first = (latNumberI * (longs+1)) + longNumberI;
+        var second = first + longs + 1;
+        _indices.push(first);
+        _indices.push(second);
+        _indices.push(first+1);
 
-            _indices.push(second);
-            _indices.push(second+1);
-            _indices.push(first+1);
-        }
+        _indices.push(second);
+        _indices.push(second+1);
+        _indices.push(first+1);
+      }
     }
-
   };
 
   var render = function() {
@@ -65,12 +64,15 @@
     gl.uniform3fv(_thetaLoc, _theta);
     gl.uniform3fv(_scaleLoc, _scale);
     gl.uniform3fv(_translateLoc, _translate);
+    gl.uniform4fv(_colorLoc, _color);
 
-    // gl.drawElements( gl.TRIANGLES, _indices.length, gl.UNSIGNED_SHORT, 0 );
     gl.drawElements( gl.LINE_LOOP, _indices.length, gl.UNSIGNED_SHORT, 0 );
   };
 
   var updateSettings = function(evt) {
+    if (evt.target.id === 'shapeColor') {
+      _color = ColorUtils.hexToGLvec4(document.getElementById('shapeColor').value);
+    }
     if (evt.target.id === 'rotateX') {
       _theta[0] = evt.target.valueAsNumber;
     }
@@ -131,16 +133,6 @@
       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
       gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(_indices), gl.STATIC_DRAW);
 
-      // Load color data buffer onto GPU
-      var cBuffer = gl.createBuffer();
-      gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-      gl.bufferData( gl.ARRAY_BUFFER, flatten(_vertexColors), gl.STATIC_DRAW );
-
-      // Associate shader variables with color data buffer
-      var vColor = gl.getAttribLocation( _program, 'vColor' );
-      gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
-      gl.enableVertexAttribArray( vColor );
-
       // Load vertex buffer onto GPU
       var vBuffer = gl.createBuffer();
       gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
@@ -152,6 +144,7 @@
       gl.enableVertexAttribArray( vPosition );
 
       // Uniform vars for user specified parameters
+      _colorLoc = gl.getUniformLocation(_program, 'fColor');
       _thetaLoc = gl.getUniformLocation(_program, 'theta');
       _scaleLoc = gl.getUniformLocation(_program, 'scale');
       _translateLoc = gl.getUniformLocation(_program, 'translate');
@@ -163,7 +156,7 @@
 
   window.App = App;
 
-}(window));
+}(window, window.ColorUtils));
 
 
 /**
