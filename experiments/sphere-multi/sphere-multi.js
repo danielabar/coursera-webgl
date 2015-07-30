22e00d95ac08dd556e8c589ea0f9e393bd0fb63c
Daniela Baron <dbaron@uncharted.software>
2015-07-30 14:00:30 -0400

    remove unused code

--- experiments/sphere-multi/sphere-multi.js
+++ experiments/sphere-multi/sphere-multi.js
@@ -5,21 +5,9 @@
   'use strict';
 
   var gl,
-    _program,
     _canvas,
-    _vertices = [],
-    _indices = [],
-    _color = vec4(1.0, 0.0, 0.0, 1.0),
-    _colorLoc,
-    _theta = [30, 60, 0],
-    _thetaLoc,
-    _scale = [1, 1, 1],
-    _scaleLoc,
-    _translate = [0, 0, 0],
-    _translateLoc,
     _shapes = [];
 
-  // https://www.webkit.org/blog-files/webgl/Earth.html
   var drawSphere = function(radius) {
     var lats = 30,
       longs = 30,
@@ -104,17 +92,6 @@
     });
   };
 
-  var render = function() {
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    // gl.uniform3fv(_thetaLoc, _theta);
-    // gl.uniform3fv(_scaleLoc, _scale);
-    // gl.uniform3fv(_translateLoc, _translate);
-    // gl.uniform4fv(_colorLoc, _color);
-    //
-    // gl.drawElements( gl.LINE_LOOP, _indices.length, gl.UNSIGNED_SHORT, 0 );
-  };
-
   var addShape = function() {
     var shape = {},
       shapeVI;
@@ -152,41 +129,6 @@
     }
   };
 
-  // var updateSettings = function(evt) {
-  //   if (evt.target.id === 'shapeColor') {
-  //     _color = ColorUtils.hexToGLvec4(document.getElementById('shapeColor').value);
-  //   }
-  //   if (evt.target.id === 'rotateX') {
-  //     _theta[0] = evt.target.valueAsNumber;
-  //   }
-  //   if (evt.target.id === 'rotateY') {
-  //     _theta[1] = evt.target.valueAsNumber;
-  //   }
-  //   if (evt.target.id === 'rotateZ') {
-  //     _theta[2] = evt.target.valueAsNumber;
-  //   }
-  //   if (evt.target.id === 'scaleX') {
-  //     _scale[0] = evt.target.valueAsNumber;
-  //   }
-  //   if (evt.target.id === 'scaleY') {
-  //     _scale[1] = evt.target.valueAsNumber;
-  //   }
-  //   if (evt.target.id === 'scaleZ') {
-  //     _scale[2] = evt.target.valueAsNumber;
-  //   }
-  //   if (evt.target.id === 'translateX') {
-  //     _translate[0] = evt.target.valueAsNumber;
-  //   }
-  //   if (evt.target.id === 'translateY') {
-  //     _translate[1] = evt.target.valueAsNumber;
-  //   }
-  //   if (evt.target.id === 'translateZ') {
-  //     _translate[2] = evt.target.valueAsNumber;
-  //   }
-  //
-  //   render();
-  // };
-
   var App = {
 
     init: function() {
@@ -197,41 +139,12 @@
       if ( !gl ) { alert( 'WebGL isn\'t available' ); }
 
       // Register settings event handlers
-      // document.getElementById('settings').addEventListener('change', updateSettings);
       document.getElementById('settings').addEventListener('click', update);
 
       // Configure WebGL
       gl.viewport( 0, 0, _canvas.width, _canvas.height );
       gl.clearColor(0.0, 0.0, 0.0, 1.0);
       gl.enable(gl.DEPTH_TEST);
-
-      // Initialize data arrays
-      // drawSphere(0.8);
-
-      // Load shaders
-      // _program = initShaders( gl, 'vertex-shader', 'fragment-shader' );
-      // gl.useProgram( _program );
-
-      // Load index data onto GPU
-      // var iBuffer = gl.createBuffer();
-      // gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
-      // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(_indices), gl.STATIC_DRAW);
-
-      // Load vertex buffer onto GPU
-      // var vBuffer = gl.createBuffer();
-      // gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-      // gl.bufferData( gl.ARRAY_BUFFER, flatten(_vertices), gl.STATIC_DRAW );
-
-      // Associate shader variables with vertex data buffer
-      // var vPosition = gl.getAttribLocation( _program, 'vPosition' );
-      // gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
-      // gl.enableVertexAttribArray( vPosition );
-
-      // Uniform vars for user specified parameters
-      // _colorLoc = gl.getUniformLocation(_program, 'fColor');
-      // _thetaLoc = gl.getUniformLocation(_program, 'theta');
-      // _scaleLoc = gl.getUniformLocation(_program, 'scale');
-      // _translateLoc = gl.getUniformLocation(_program, 'translate');
 
       renderAll(_shapes);
     }
