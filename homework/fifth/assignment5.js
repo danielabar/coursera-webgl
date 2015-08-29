b36a710553b6e49ee0778c20f804c729f02059c8
Daniela Baron <baron.daniela@gmail.com>
2015-08-29 14:15:22 -0400

    rotation animation

--- homework/fifth/assignment5.js
+++ homework/fifth/assignment5.js
@@ -14,7 +14,10 @@
     modelViewMatrix = mat4(),
     projectionMatrix = mat4(),
     shapeColor = vec4(1.0, 0.0, 0.0, 1.0),
-    texture;
+    texture,
+    mouseDown = false,
+    lastMouseX = null,
+    lastMouseY = null;
 
   var configureTexture = function(image) {
     texture = gl.createTexture();
@@ -37,11 +40,17 @@
     // gl.uniformMatrix4fv(uProjection, false, flatten(perspective));
   };
 
+  var rotateModelView = function() {
+    theta[0] += 1.0;
+    theta[1] += 0.1;
+    modelViewMatrix = buildModelViewMatrix();
+  };
+
   var render = function() {
     adjustCanvas();
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    modelViewMatrix = buildModelViewMatrix(viewMatrix, theta);
+    rotateModelView();
     gl.uniformMatrix4fv(gl.getUniformLocation(program, 'modelViewMatrix' ), false, flatten(modelViewMatrix) );
 
     // if switch to perspective then this will be done in adjustCanvas
@@ -79,7 +88,7 @@
     return lookAt(eye, at, up);
   };
 
-  var buildModelViewMatrix = function(viewMatrix, theta) {
+  var buildModelViewMatrix = function() {
     var modelMatrix = mat4();
 
     modelMatrix = mult(modelMatrix, rotate(theta[0], [1, 0, 0] ));
@@ -88,6 +97,42 @@
 
     return mult(modelMatrix, viewMatrix);
   };
+
+  // var handleMouseDown = function(evt) {
+  //   mouseDown = true;
+  //   lastMouseX = evt.clientX;
+  //   lastMouseY = evt.clientY;
+  // };
+  //
+  // var handleMouseUp = function() {
+  //   console.log('mouse up');
+  //   mouseDown = false;
+  // };
+  //
+  // var handleMouseMove = function(evt) {
+  //   if (!mouseDown) {
+  //     return;
+  //   }
+  //   console.log('mouse move');
+  //   var newX = evt.clientX;
+  //   var newY = evt.clientY;
+  //
+  //   var deltaX = newX - lastMouseX;
+  //   theta[1] = radians(deltaX / 10);
+  //   // var newRotationMatrix = mat4.create();
+  //   // mat4.identity(newRotationMatrix);
+  //   // mat4.rotate(newRotationMatrix, degToRad(deltaX / 10), [0, 1, 0]);
+  //
+  //   var deltaY = newY - lastMouseY;
+  //   theta[0] = radians(deltaY / 10);
+  //   // mat4.rotate(newRotationMatrix, degToRad(deltaY / 10), [1, 0, 0]);
+  //   // mat4.multiply(newRotationMatrix, moonRotationMatrix, moonRotationMatrix);
+  //
+  //   lastMouseX = newX;
+  //   lastMouseY = newY;
+  //
+  //   modelViewMatrix = buildModelViewMatrix();
+  // };
 
   var App = {
 
@@ -98,7 +143,12 @@
       gl = WebGLUtils.setupWebGL( canvas, {preserveDrawingBuffer: true} );
       if ( !gl ) { alert( 'WebGL isn\'t available' ); }
 
-      // TODO register mouse handlers to manipulate theta
+      // Register mouse handlers for interaction
+      // canvas.onmousedown = handleMouseDown;
+      // canvas.onmouseup = handleMouseUp;
+      // canvas.onmousemove = handleMouseMove;
+      // document.onmouseup = handleMouseUp;
+      // document.onmousemove = handleMouseMove;
 
       // Configure WebGL
       gl.viewport( 0, 0, canvas.width, canvas.height );
@@ -112,6 +162,7 @@
       shape = Sphere.generate();
       viewMatrix = buildViewMatrix();
       projectionMatrix = buildProjectionMatrix();
+      modelViewMatrix = buildModelViewMatrix();
 
       // Load shaders
       program = initShaders(gl, 'vertex-shader', 'fragment-shader');
