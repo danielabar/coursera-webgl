67e233930b60c26521dc47c31d0849db4ad9705f
Daniela Baron <baron.daniela@gmail.com>
2015-08-30 19:34:54 -0400

    attempt to toggle reflection in main assignment

--- homework/fifthb/assignment5b.js
+++ homework/fifthb/assignment5b.js
@@ -31,7 +31,6 @@ var lightAmbient = vec4(0.2, 0.2, 0.2, 1.0 );
 var lightDiffuse = vec4( 1.0, 1.0, 1.0, 1.0 );
 var lightSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
 
-// var materialAmbient = vec4( 1.0, 0.0, 1.0, 1.0 );
 var materialAmbient = vec4( 1.0, 1.0, 1.0, 1.0 );
 var materialDiffuse = vec4( 1.0, 0.8, 0.0, 1.0 );
 var materialSpecular = vec4( 1.0, 1.0, 1.0, 1.0 );
