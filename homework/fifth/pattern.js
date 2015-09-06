617b2c0ff7eb349c8f90b08f7ed4695c105b7103
Daniela Baron <baron.daniela@gmail.com>
2015-09-06 09:15:29 -0400

    stripe pattern

--- homework/fifth/pattern.js
+++ homework/fifth/pattern.js
@@ -63,6 +63,35 @@
       return imageData;
     },
 
+    stripe: function(texSize) {
+      var c,
+        imageData = new Uint8Array(4*texSize*texSize),
+        numChecksX = 30,
+        numChecksY = 1,
+        patchx,
+        patchy;
+
+      for ( var i = 0; i < texSize; i++ ) {
+        for ( var j = 0; j <texSize; j++ ) {
+          patchx = Math.floor(i/(texSize/numChecksX));
+          patchy = Math.floor(j/(texSize/numChecksY));
+
+          if (patchx%2 ^ patchy%2) {
+            c = [0, 0, getRandomInt(100, 255)];
+          } else {
+            c = [getRandomInt(100, 255), 0, 0];
+          }
+
+          imageData[4*i*texSize+4*j] = c[0];
+          imageData[4*i*texSize+4*j+1] = c[1];
+          imageData[4*i*texSize+4*j+2] = c[2];
+          imageData[4*i*texSize+4*j+3] = 255;
+        }
+      }
+
+      return imageData;
+    },
+
     sinusoid: function(texSize) {
       var imageData = new Uint8Array(4*texSize*texSize);
 
